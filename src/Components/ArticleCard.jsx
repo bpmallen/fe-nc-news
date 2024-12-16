ca8a5e9a805f8d6e2e9d3e1365a6407cc6eec9cb
bpmallen <bpmallen@gmx.com>
2024-12-16 16:24:35 +0000

    4 list all articles

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
+
 const ArticleCard = ({ article }) => {
-  //   console.log(article);
+  console.log(article);
   return (
     <li className="article-card">
       <p className="article-title">{article.title}</p>
       <div className="article-flex">
-        <img
-          className="article-img"
-          src={article.article_img_url}
-          alt={article.title}
-        />
+        <Link to={`/article/${article.article_id}`}>
+          <img
+            className="article-img"
+            src={article.article_img_url}
+            alt={article.title}
+          />
+        </Link>
         <p className="article-author">Author: {article.author}</p>
         <p className="article-topic">Topic: {article.topic}</p>
       </div>
