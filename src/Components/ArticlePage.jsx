3c04a2632c36d7c136d60a56b16af2a7078788c0
bpmallen <bpmallen@gmx.com>
2024-12-17 10:37:29 +0000

    5 view individual article

--- src/Components/ArticlePage.jsx
+++ src/Components/ArticlePage.jsx
@@ -1,37 +1,47 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getArticleById } from "../api/api";
+import Error from "./Error";
 
 const ArticlePage = () => {
   const { id } = useParams();
-  console.log(useParams());
-  const [article, setArticle] = useState(null);
+  const [article, setArticle] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    fetch(`https://my-nc-news-yyln.onrender.com/api/articles/${id}`)
-      .then((response) => {
-        return response.json();
-      })
+    setIsError(false);
+    setLoading(true);
+    getArticleById(id)
       .then((data) => {
-        console.log(data.article);
-        setArticle(data.article);
+        setArticle(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setIsError(true);
+        setError(error);
+        setLoading(false);
       });
-  }, [id]); // this fetches the article if the id changes
+  }, [id]);
 
-  if (!article) {
-    return <div>Loading...</div>;
+  if (loading) {
+    return <div className="loading-container">Loading, please wait...</div>;
   }
 
+  if (isError) return <Error error={error} />;
+
   return (
-    <div>
-      <h2 className="article-title">{article.title}</h2>
+    <div className="article-page">
+      <h2 className="article-page-title">{article.title}</h2>
       <img
-        className="article-img"
+        className="article-page-img"
         src={article.article_img_url}
         alt={article.title}
       />
-      <p className="article-author">Author: {article.author}</p>
-      <p className="article-topic">Topic: {article.topic}</p>
-      <p>{article.body}</p>
+      <p className="article-page-author">Author: {article.author}</p>
+      <p className="article-page-topic">Topic: {article.topic}</p>
+      <div className="article-page-body">{article.body}</div>
     </div>
   );
 };
